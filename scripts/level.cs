800cbacbc81670325226c97b2e7e10bb4cc77d23
Gianni <gianni.gagliardi@stud.uni-heidelberg.de>
2023-12-23 15:48:42 +0100

    bugfix and increased readability

--- scripts/level.cs
+++ scripts/level.cs
@@ -1,43 +1,47 @@
 using Godot;
 using System;
-using System.Linq;
-using System.Numerics;
+
 
 
 
 public partial class level : Node3D
 {
-	public Godot.Vector3 rayOrigin = Godot.Vector3.Zero;
-	public Godot.Vector3 rayTarget = Godot.Vector3.Zero;
-	private const double JoystickSensitivity = 0.2f;
+	private const double JOYSTICKSENSITIVITY = 0.2f; 
+	private const float RAYLENGHT = 2000f; 
+
+	public override void _PhysicsProcess(double delta)
+	{
+
+		Godot.Vector2 mousePosition =  GetViewport().GetMousePosition();
+
+		//Code for Player-Rotation with mouse
+		if (IsMouseInViewport(mousePosition))   
+		{
+			ProcessMouseRotation(mousePosition);
+		}
+
+		ProcessJoystickRotation();
+	}
 
 	private bool IsMouseInViewport(Godot.Vector2 mousePosition)    //important to check if mouse is in viewport
 	{
 		return mousePosition.X >= 0 && mousePosition.Y >= 0 &&
 			   mousePosition.X < GetViewport().GetVisibleRect().Size.X &&
-			   mousePosition.Y < GetViewport().GetVisibleRect().Size.Y;
+			   mousePosition.Y < GetViewport().GetVisibleRect().Size.Y;   //assertion
 	}
-	public override void _PhysicsProcess(double delta)
+
+	private void ProcessMouseRotation(Godot.Vector2 mousePosition)
 	{
-
-		Godot.Vector2 mousePosition =  GetViewport().GetMousePosition();
-		CsgBox3D ground = GetNode<CsgBox3D>("Ground");
-
-		//Code for Player-Rotation with mouse
-		if (IsMouseInViewport(mousePosition))
-		{
-			
-			
-
+			Godot.Vector3 rayOrigin = Godot.Vector3.Zero;
+			Godot.Vector3 rayTarget = Godot.Vector3.Zero;
+			CsgBox3D ground = GetNode<CsgBox3D>("Ground");
 			Camera3D camera = GetNode<Camera3D>("Camera3D");		//Our Camera3D child Node
 			rayOrigin = camera.ProjectRayOrigin(mousePosition);
-			
-
-			rayTarget = rayOrigin + camera.ProjectRayNormal(mousePosition) * 2000;  //extended ray
+			rayTarget = rayOrigin + camera.ProjectRayNormal(mousePosition) * RAYLENGHT;  //extended ray
 
 			PhysicsDirectSpaceState3D spaceState = GetWorld3D().DirectSpaceState;
 
-			var rayQueryParameters = new PhysicsRayQueryParameters3D           //needed to get the intersection
+			var rayQueryParameters = new PhysicsRayQueryParameters3D           //needed to get the intersection, null beachten, try-catch
 			{
 				From = rayOrigin,
 				To = rayTarget
@@ -46,25 +50,27 @@ public partial class level : Node3D
 
 			var intersection = spaceState.IntersectRay(rayQueryParameters);
 
-			if (intersection != null && intersection.ContainsKey("collider"))  
+			if (intersection != null && intersection.ContainsKey("collider"))    //collision mit spieler beachten
 			{
 				GodotObject collider = (GodotObject) intersection["collider"];
 				Godot.Vector3 pos = (Godot.Vector3) intersection["position"];
+				Player player = GetNode<Player>("Player");
 				
-				if (collider != null && (CsgBox3D)collider == ground)   // making sure that only collison with ground is recognized
+				if (collider != null && collider!= player && (CsgBox3D)collider == ground)   // making sure that only collison with ground is recognized
 				{
-					Player player = GetNode<Player>("Player");
-					Godot.Vector3 lookAtMe = new Godot.Vector3(pos.X, player.Position.Y , pos.Z);
+					Godot.Vector3 lookAtMe = new Godot.Vector3(pos.X, player.Position.Y , pos.Z);  //new überprüfung
 					player.LookAt(lookAtMe, Godot.Vector3.Up);
 				}
 			}
-		}
+	}
 
-	 	//Code for Player-Rotation with joystick
-		float joystickRightX = Input.GetJoyAxis(0, JoyAxis.RightX);
+	private void ProcessJoystickRotation()
+	{
+		//Code for Player-Rotation with joystick
+		float joystickRightX = Input.GetJoyAxis(0, JoyAxis.RightX);   //if abfrage sparen mit sensitivity untermethode in der abgefragt wird
 		float joystickRightY = Input.GetJoyAxis(0, JoyAxis.RightY);
 
-		if (Math.Abs(joystickRightX) > JoystickSensitivity || Math.Abs(joystickRightY) > JoystickSensitivity)
+		if (Math.Abs(joystickRightX) > JOYSTICKSENSITIVITY|| Math.Abs(joystickRightY) > JOYSTICKSENSITIVITY)
 		{
 			Player player = GetNode<Player>("Player");
 			// calculating new View out of right-joystick direction
