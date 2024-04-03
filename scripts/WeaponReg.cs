cef17a83350900d247be07b311f22ad44eaf4063
Doeniel Laptop <berndtda@t-online.de>
2024-04-03 20:11:35 +0200

    init weapon system

---
+++ scripts/WeaponReg.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class WeaponReg : Node
+{
+    static Dictionary<string, PackedScene> _equipedWeapons = new();
+    static Dictionary<string, PackedScene> _worldWeapons = new();
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+        _equipedWeapons.Add("weapon_start_weapon", GD.Load<PackedScene>("res://weapon.tscn"));
+    }
+
+	public static PackedScene GetEquipedWeapon(string name){
+        return _equipedWeapons[name];
+    }
+
+	public static PackedScene GetWorldWeapon(string name){
+        return _worldWeapons[name];
+    }
+}
---
+++ scripts/WorldItem.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class WorldItem : Area3D
+{
+    [Export]
+    string _name = "untitled";
+	
+}
